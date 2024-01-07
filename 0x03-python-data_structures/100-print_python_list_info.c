ddf3531562ee48c0d9b31fc394a92bbbdcfc6e24
EsraaGamal-eg <esraa.g.moheyeldin@gmail.com>
2024-01-07 08:26:00 -0800

    all

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <Python.h>
-/**
- * print_python_list_info -  function that prints some basic
- *							info about Python lists
- * @p: python list
- */
-void print_python_list_info(PyObject *p)
+
+void print_python_list_info(PyObject*p)
 {
 	int elem;
 
@@ -14,3 +9,5 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 	for (elem = 0; elem < Py_SIZE(p); elem++)
 		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+}
+
